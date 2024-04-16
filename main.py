f6832689b17294bacdac4aa98b88aa6bf77550f8
Radoslav304 <90415819+Radoslav304@users.noreply.github.com>
2024-04-16 17:06:56 +0200

    ukladanie zachytenych tvari

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import cv2
 # Inicializacia
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')  # Pridanie detekcie úsmevu
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')  
 
 cap = cv2.VideoCapture(0)
 
@@ -19,7 +19,7 @@ while (cap.isOpened()):
 
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    for (x, y, w, h) in faces:
+    for i, (x, y, w, h) in enumerate(faces):
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 1)
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = frame[y:y + h, x:x + w]
@@ -28,10 +28,13 @@ while (cap.isOpened()):
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 1)
         
-        smiles = smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.7, minNeighbors=22, minSize=(25, 25))  # Detekcia úsmevu
+        smiles = smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.7, minNeighbors=22, minSize=(25, 25)) 
 
         for (sx, sy, sw, sh) in smiles:
             cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 1)
+
+        # Uložiť detegovanú tvár ako obrázok
+        cv2.imwrite("detected_face_" + str(i) + ".jpg", frame[y:y+h, x:x+w])
 
     cv2.putText(frame, "Pocet tvari: " + str(len(faces)), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)  
 
