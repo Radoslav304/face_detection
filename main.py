055fc8f87b0e220c8fdbd4f11fdd8eaa6b646375
Radoslav304 <90415819+Radoslav304@users.noreply.github.com>
2024-04-16 16:15:13 +0200

    smile_detection

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import cv2
 # Inicializacia
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')  # Pridanie detekcie úsmevu
 
 cap = cv2.VideoCapture(0)
 
@@ -26,13 +27,18 @@ while (cap.isOpened()):
 
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 1)
+        
+        smiles = smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.7, minNeighbors=22, minSize=(25, 25))  # Detekcia úsmevu
+
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 1)
 
     cv2.putText(frame, "Pocet tvari: " + str(len(faces)), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)  
 
     cv2.imshow('Detekcia tváre', frame)
 
     if cv2.waitKey(10) & 0xFF == ord('q'):
-        breakq
+        break
 
 cap.release()
 cv2.destroyAllWindows()
